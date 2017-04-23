3c0cf34c8b37cd138c7cffe8c0af975f0489cbc2
victor zhang <atlantis1024@163.com>
2017-04-23 08:56:41 +0800

    添加示例

---
+++ src/examples/demo3/app/index.js
@@ -0,0 +1,6 @@
+require("./index.css");
+var sub = require('./sub');
+var app = document.createElement('div');
+app.innerHTML = '<h1>Hello World</h1>';
+app.appendChild(sub());
+document.body.appendChild(app);
